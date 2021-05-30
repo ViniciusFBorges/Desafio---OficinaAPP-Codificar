90075a0c1a2dab5f277d50d865825ba7d391a61b
Vinicius Ferreira <70003677+ViniciusFBorges@users.noreply.github.com>
2021-05-30 15:42:35 -0300

    Add files via upload

---
+++ App.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import { StyleSheet, View, Text, } from 'react-native';
+import Fetch from './src/Fetch';
+export default function OficinaAPP () {
+
+  return (
+    <View style={styles.container}>
+      <View style={styles.viewTitulo}>
+        <Text style={styles.titulo}>Selecione o Orçamento:</Text>
+      </View>
+      <View>
+        <Fetch/>
+      </View> 
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+
+  titulo: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    marginTop: 7,
+    marginBottom: 7,
+    color: '#E0E0E0'
+  },
+  container: {
+    flex: 1,
+    backgroundColor: '#212121',
+  },
+  viewTitulo: {
+    alignItems: 'center'
+  },
+})
